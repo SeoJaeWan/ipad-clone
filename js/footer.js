655f107f4ccde84d0625c7b996c8d1349c5dc8e3
SeoJaeWan <sjw7324@naver.com>
2024-11-22 10:56:00 +0900

    feat: footer

--- js/footer.js
+++ js/footer.js
@@ -24,3 +24,17 @@ import navigations from "../data/navigations.js";
     navigationEl.appendChild(mapEl);
   });
 })();
+
+(function () {
+  const thisYearEl = document.querySelector(".this-year");
+  thisYearEl.textContent = new Date().getFullYear();
+
+  const mapEls = [...document.querySelectorAll("footer .navigations .map")];
+
+  mapEls.forEach((el) => {
+    const h3El = el.querySelector("h3");
+    h3El.addEventListener("click", () => {
+      el.classList.toggle("active");
+    });
+  });
+})();
